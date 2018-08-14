edc2ac14249e470410af08cfa0c33b3d7ca2b6b2
liuweihang <liuweihang@medp.ai>
2018-08-14 20:53:27 +0800

    第一次提交

---
+++ ScreenCapturer/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ScreenCapturer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(95, 122);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(129, 50);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "截屏";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 269);
+            this.Controls.Add(this.btnCapture);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Leave += new System.EventHandler(this.MainForm_Leave);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+    }
+}
+
